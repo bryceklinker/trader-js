3510dc5979fecf2dd6e54444b40da7798839ea02
Bryce Klinker <bryce.klinker@gmail.com>
2016-09-07 23:47:53 -0500

    Added clean task to gulp build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
     uglify = require('gulp-uglify'),
+    util = require('gulp-util'),
     path = require('path'),
+    rimraf = require('rimraf'),
     Server = require('karma').Server
 
 var paths = {
     src: {
         js: path.join(__dirname, 'src/**/*.js'),
-        sass: path.join(__dirname, 'src/**/*.scss')
+        sass: path.join(__dirname, 'src/**/*.scss'),
+        html: path.join(__dirname, 'src/**/*.html')
     },
     test: {
         js: path.join(__dirname, 'src/**/*.spec.js')
@@ -16,24 +19,34 @@ var paths = {
     dist: {
         indexjs: 'index.min.js',
         js: path.join(__dirname, 'dist/js'),
-        css: path.join(__dirname, 'dist/css')
+        css: path.join(__dirname, 'dist/css'),
+        root: path.join(__dirname, 'dist')
     }
 }
 
-gulp.task('build:js', function() {
+gulp.task('build:js', ['clean'], function() {
     return gulp.src([paths.src.js, '!' + paths.test.js])
         .pipe(concat(paths.dist.indexjs))
         .pipe(uglify())
         .pipe(gulp.dest(paths.dist.js))
 })
 
-gulp.task('build:sass', function() {
+gulp.task('build:sass', ['clean'], function() {
     return gulp.src(paths.src.sass)
         .pipe(sass())
         .pipe(gulp.dest(paths.dist.css))
 })
 
-gulp.task('build', ['build:js', 'build:sass'])
+gulp.task('copy:html', ['clean'], function() {
+    return gulp.src(paths.src.html)
+        .pipe(gulp.dest(paths.dist.root))
+})
+
+gulp.task('clean', function(cb) {
+    rimraf(paths.dist.root, cb)
+})
+
+gulp.task('build', ['clean', 'build:js', 'build:sass', 'copy:html'])
 
 gulp.task('unit:test', ['build'], function(done) {
     new Server({
@@ -48,4 +61,4 @@ gulp.task('watch', ['build', 'unit:test'], function(){
     gulp.watch(paths.test.js, ['unit:test'])
 })
 
-gulp.task('default', ['build', 'unit:test'])
+gulp.task('default', ['clean', 'build', 'unit:test'])
